15153c67429a60fe31fb55db23d7d35822335736
Dai Phong <secret2xbtc@gmail.com>
2020-01-05 15:43:29 +0700

    all

---
+++ src/main/java/com/phongdat/pirago/ExampleCiCdTestingApplication.java
@@ -0,0 +1,13 @@
+package com.phongdat.pirago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleCiCdTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleCiCdTestingApplication.class, args);
+	}
+
+}
